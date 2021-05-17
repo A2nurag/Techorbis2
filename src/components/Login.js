92b609dc211e08e35d366eaf15ad1681a7ac6989
A2nurag <anurag2sarkar@gmail.com>
2021-05-17 21:46:33 +0530

    changed some design and stuff

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,23 +1,25 @@
-import { Button } from '@material-ui/core'
-import React from 'react'
-import './Login.css'
-import { auth, provider } from '../firebase'
+import { Button } from "@material-ui/core";
+import React from "react";
+import "./Login.css";
+import { auth, provider } from "../firebase";
 
 function Login() {
-    const signIn = () => {
-        auth.signInWithPopup(provider)
-    }
+  const signIn = () => {
+    auth.signInWithPopup(provider);
+  };
 
-    return (
-        <div className="login">
-            <div className="login__header">
-                <h1>Welcome to Techorbis 2.0</h1>
-            </div>
-            <div className="login__button">
-                <Button onClick={signIn}>Sign IN</Button>
-            </div>
-        </div>
-    )
+  return (
+    <div className="login">
+      <div className="login__header">
+        <h3>Welcome to</h3>
+        <h1>Techorbis 2.0</h1>
+      </div>
+
+      <div className="login__button">
+        <Button onClick={signIn}>Sign IN</Button>
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
