799f11a1b56f488f51c46f16c4719b577cdd8287
Karam Sahoo <karamsahoo@pop-os.localdomain>
2021-05-20 12:37:32 +0530

    Quiz gets over code.

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -5,30 +5,30 @@ import mntclogo from "../assets/logo.png";
 import { Link } from "react-router-dom";
 
 function Intro() {
-  return (
-    <div className="intro">
-      <div className="intro__header">
-        <h3>Welcome to</h3>
-        <h1>Techorbis 2.0</h1>
-      </div>
+    return (
+        <div className="intro">
+            <div className="intro__header">
+                <h3>Welcome to</h3>
+                <h1>Techorbis 2.0</h1>
+            </div>
 
-      <div className="intro__button">
-        <Link to="/questions">
-          <Button>Go to Questions</Button>
-        </Link>
+            <div className="intro__button">
+                <Link to="/questions">
+                    <Button>Go to Questions</Button>
+                </Link>
         or
         <Link to="/rules" className="rules-link">
-          Check the Rules
+                    Check the Rules
         </Link>
-      </div>
+            </div>
 
-      <div className="intro__footer">
-        <h4>
-          Developed by the <img src={mntclogo}></img> Team
+            <div className="intro__footer">
+                <h4>
+                    Developed by the <img src={mntclogo}></img> Team
         </h4>
-      </div>
-    </div>
-  );
+            </div>
+        </div>
+    );
 }
 
 export default Intro;
